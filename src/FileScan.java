09bb782d96631fe48394010c4411c7865c041aaa
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-12-03 17:04:59 -0500

    primary code parts should be working

--- src/FileScan.java
+++ src/FileScan.java
@@ -9,34 +9,62 @@ import java.util.Scanner;
 public class FileScan {
 
         public static void main(String[] args) {
-            File selectedFile = null;
+            int numberOfLines = 0;
+            int numberOfWords = 0;
+            int numberOfChars = 0;
+            String line = "";
+
+            Scanner inFile;
+
             if (args.length > 0) {
-            selectedFile = new File("src", args[0]);
+
+            File selectedFile = new File("src", args[0]);
+
             if (!selectedFile.exists() || !selectedFile.isFile()) {
+
                 System.out.println("File not found, or the File does not exist");
+
             }
             else {
-                System.out.println("File found");
+                try {
+                    System.out.println("File " + selectedFile.getName() + " found");
+                    inFile = new Scanner(selectedFile);
+                    while (inFile.hasNextLine()) {
+                        line = inFile.nextLine();
+                        System.out.println(line);
+                        numberOfLines++;
+                        for (String words : line.split(" ")) {
+                            numberOfWords++;
+                        }
+                        numberOfChars = numberOfChars + line.length();
+
+
+                    }
+                    inFile.close();
+                }
+                catch (FileNotFoundException e) {
+                    System.out.println("File Not Found Error");
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    System.out.println("IOException Error");
+                    e.printStackTrace();
+                }
+
+
             }
+
             }
             else
             {
-
                 JFileChooser chooser = new JFileChooser();
-                Scanner inFile;
-                String line = "";
                 Path target = new File(System.getProperty("user.dir")).toPath();
                 target = target.resolve("src");
                 chooser.setCurrentDirectory(target.toFile());
-                int numberOfLines = 0;
-                int numberOfWords = 0;
-                int numberOfChars = 0;
 
                 try {
 
                     if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                         target = chooser.getSelectedFile().toPath();
-
                         inFile = new Scanner(target);
 
                         while (inFile.hasNextLine()) {
@@ -47,12 +75,11 @@ public class FileScan {
                                 numberOfWords++;
                             }
                             numberOfChars = numberOfChars + line.length();
+                            System.out.println("File " + target.getFileName() + " successfully read!");
+
                         }
 
-                        System.out.println("File " + target.getFileName() + " successfully read!");
-                        System.out.println("The text file has " + numberOfLines + " lines");
-                        System.out.println("The text file has " + numberOfWords + " words");
-                        System.out.println("The text file has " + numberOfChars + " characters");
+
 
                         inFile.close();
 
@@ -68,6 +95,11 @@ public class FileScan {
                     e.printStackTrace();
                 }
             }
+
+            System.out.println("The text file has " + numberOfLines + " lines");
+            System.out.println("The text file has " + numberOfWords + " words");
+            System.out.println("The text file has " + numberOfChars + " characters");
+
 
 
         }
